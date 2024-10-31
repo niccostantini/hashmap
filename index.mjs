d7f88f3734c001cc92cae293afa8ad0cbb55fb49
niccostantini <65295160+niccostantini@users.noreply.github.com>
2024-10-31 15:00:51 +0100

    Update testing for HashSets

--- index.mjs
+++ index.mjs
@@ -1,24 +1,15 @@
 import { Node, LinkedList } from "./linkedList.mjs";
 import HashMap from "./hashMap.mjs"
+import HashSet from "./hashSet.mjs";
 
-let test = new HashMap();
+let test = new HashSet();
 
-test.set('apple', 'red')
-test.set('banana', 'yellow')
-test.set('carrot', 'orange')
-test.set('dog', 'brown')
-test.set('elephant', 'gray')
-test.set('frog', 'green')
-test.set('grape', 'purple')
-test.set('hat', 'black')
-test.set('ice cream', 'white')
-test.set('jacket', 'blue')
-test.set('kite', 'pink')
-test.set('lion', 'golden')
+test.set('apple')
+test.set('banana')
+test.set('carrot')
 
-console.log(test.get("lion"))
-test.set("lion", "crap")
-test.set('moon', 'silver')
+console.log(test.has("lion"))
+
 
 console.log(test)
-console.log(test.entries())
+console.log(test.keys())
